8d3af4dcf3e42d65c474d2d862b7ef52aa91d212
gulipa29 <minecraft.met29@gmail.com>
2025-02-16 18:29:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -198,10 +198,19 @@ def keep_alive():
 threading.Thread(target=keep_alive, daemon=True).start()
 
 
-@app.route("/")
-def home():
-    return "Server is running!", 200
-
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
