15e84901dc4d35c8e350a758bb0926aba036c350
gulipa29 <minecraft.met29@gmail.com>
2025-02-16 11:09:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+import googletrans
 import tempfile, os
 import datetime
 import openai
